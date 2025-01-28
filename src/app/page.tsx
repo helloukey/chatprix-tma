c2ecc62b42994455cc42c5b9f2e91aa19dc412bd
Kunal <kunalukey32@gmail.com>
2025-01-28 20:58:44 +0530

    added navbar with placeholder settings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,47 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Bolt } from "lucide-react";
+
+const SettingsDrawer = () => {
+  return (
+    <Drawer>
+      <DrawerTrigger>
+        <Bolt />
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+          <DrawerDescription>This action cannot be undone.</DrawerDescription>
+        </DrawerHeader>
+        <DrawerFooter>
+          <Button>Submit</Button>
+          <DrawerClose>
+            <Button variant="outline">Cancel</Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
 export default function Home() {
   return (
-    <div>
-      <h1>This is a Homepage</h1>
+    <div className="w-full p-4">
+      {/* Navbar */}
+      <div className="w-full flex justify-end items-center">
+        <SettingsDrawer />
+      </div>
     </div>
   );
 }
