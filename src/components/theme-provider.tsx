a5d109b3465b16321750085f685df4bddad51087
Kunal <kunalukey32@gmail.com>
2025-01-28 18:57:15 +0530

    added tma sdk init inside useEffect to catch window object

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { backButton, init } from "@telegram-apps/sdk-react";
+import { ComponentProps, useEffect } from "react";
 
 export function ThemeProvider({
   children,
   ...props
-}: React.ComponentProps<typeof NextThemesProvider>) {
-  // Initialize the package.
-  init();
-  backButton.mount();
+}: ComponentProps<typeof NextThemesProvider>) {
+  useEffect(() => {
+    // Initialize the package.
+    init();
+    backButton.mount();
+  }, []);
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
