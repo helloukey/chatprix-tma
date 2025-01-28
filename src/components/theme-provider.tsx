00ba0dd8aad498caa6bb81280a042f027e4b0953
Kunal <kunalukey32@gmail.com>
2025-01-28 19:46:48 +0530

    removed tma sdk init

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,17 +1,11 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { backButton, init } from "@telegram-apps/sdk-react";
-import { ComponentProps, useEffect } from "react";
+import { ComponentProps } from "react";
 
 export function ThemeProvider({
   children,
   ...props
 }: ComponentProps<typeof NextThemesProvider>) {
-  useEffect(() => {
-    // Initialize the package.
-    init();
-    backButton.mount();
-  }, []);
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
