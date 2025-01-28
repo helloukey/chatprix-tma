0ffff79df6ac816017f5ecbcf307c10b5949c538
Kunal <kunalukey32@gmail.com>
2025-01-29 00:55:22 +0530

    added placeholder profile settings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
 "use client";
 
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
 import {
   Drawer,
   DrawerClose,
@@ -11,9 +21,26 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
-import { Bolt } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { cn, countries } from "@/lib/utils";
+import { Popover } from "@radix-ui/react-popover";
+import { format } from "date-fns";
+import {
+  Bolt,
+  Calendar as CalendarIcon,
+  Check,
+  ChevronsUpDown,
+} from "lucide-react";
+import { useState } from "react";
 
 const SettingsDrawer = () => {
+  const [date, setDate] = useState<Date>();
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+
   return (
     <Drawer>
       <DrawerTrigger>
@@ -21,14 +48,121 @@ const SettingsDrawer = () => {
       </DrawerTrigger>
       <DrawerContent>
         <DrawerHeader>
-          <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-          <DrawerDescription>This action cannot be undone.</DrawerDescription>
+          <DrawerTitle>Profile</DrawerTitle>
+          <DrawerDescription>
+            Customize your profile to attract others
+          </DrawerDescription>
         </DrawerHeader>
-        <DrawerFooter>
-          <Button>Submit</Button>
+        <div className="w-full flex flex-col justify-center gap-4 px-4 my-8">
+          <Avatar className="mx-auto">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CP</AvatarFallback>
+          </Avatar>
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label htmlFor="email">Username</Label>
+            <Input type="text" id="username" placeholder="johndoe" />
+          </div>
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label htmlFor="email">Gender</Label>
+            <Tabs defaultValue="" className="w-full">
+              <TabsList className="w-full">
+                <TabsTrigger value="male" className="w-full">
+                  Male
+                </TabsTrigger>
+                <TabsTrigger value="female" className="w-full">
+                  Female
+                </TabsTrigger>
+                <TabsTrigger value="other" className="w-full">
+                  Other
+                </TabsTrigger>
+              </TabsList>
+            </Tabs>
+          </div>
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label htmlFor="email">Date of Birth</Label>
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button
+                  variant={"outline"}
+                  className={cn(
+                    "w-full justify-start text-left font-normal",
+                    !date && "text-muted-foreground"
+                  )}
+                >
+                  <CalendarIcon className="mr-2 h-4 w-4" />
+                  {date ? format(date, "PPP") : <span>Date of Birth</span>}
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="w-auto p-0">
+                <Calendar
+                  mode="single"
+                  selected={date}
+                  onSelect={setDate}
+                  initialFocus
+                />
+              </PopoverContent>
+            </Popover>
+          </div>
+          {/* Country */}
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label htmlFor="email">Country</Label>
+            <Popover open={open} onOpenChange={setOpen}>
+              <PopoverTrigger asChild>
+                <Button
+                  variant="outline"
+                  role="combobox"
+                  aria-expanded={open}
+                  className="w-full justify-between"
+                >
+                  {value
+                    ? countries.find((country) => country.value === value)
+                        ?.label
+                    : "Select country..."}
+                  <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="w-[200px] p-0">
+                <Command>
+                  <CommandInput placeholder="Search framework..." />
+                  <CommandList>
+                    <CommandEmpty>No country found.</CommandEmpty>
+                    <CommandGroup>
+                      {countries.map((country) => (
+                        <CommandItem
+                          key={country.value}
+                          value={country.value}
+                          onSelect={(currentValue) => {
+                            setValue(
+                              currentValue === value ? "" : currentValue
+                            );
+                            setOpen(false);
+                          }}
+                        >
+                          <Check
+                            className={cn(
+                              "mr-2 h-4 w-4",
+                              value === country.value
+                                ? "opacity-100"
+                                : "opacity-0"
+                            )}
+                          />
+                          {country.label}
+                        </CommandItem>
+                      ))}
+                    </CommandGroup>
+                  </CommandList>
+                </Command>
+              </PopoverContent>
+            </Popover>
+          </div>
+        </div>
+        <DrawerFooter className="flex-row">
           <DrawerClose asChild>
-            <Button variant="outline">Cancel</Button>
+            <Button variant="outline" className="w-full">
+              Cancel
+            </Button>
           </DrawerClose>
+          <Button className="w-full">Update Profile</Button>
         </DrawerFooter>
       </DrawerContent>
     </Drawer>
