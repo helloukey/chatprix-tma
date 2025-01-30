4ebd63543c345b555829137def5747eb151589db
Kunal <kunalukey32@gmail.com>
2025-01-30 21:07:12 +0530

    added particles for search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,16 +1,19 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { ParticlesWrapper } from "@/screens/home";
 import { LottieSearch } from "@/screens/search";
 
 export default function Search() {
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center p-4">
-      <LottieSearch />
-      <p className="text-muted-foreground text-center mb-8">
-        Searching for Partner...
-      </p>
-      <Button className="w-full">Cancel</Button>
-    </div>
+    <ParticlesWrapper>
+      <div className="w-full h-full flex flex-col justify-center items-center p-4">
+        <LottieSearch />
+        <p className="text-muted-foreground text-center mb-8">
+          Searching for Partner...
+        </p>
+        <Button className="w-full">Cancel</Button>
+      </div>
+    </ParticlesWrapper>
   );
 }
