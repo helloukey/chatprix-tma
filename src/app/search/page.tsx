d8d812985cc5acac650a4489da436e6e03d27f6f
Kunal <kunalukey32@gmail.com>
2025-01-30 16:44:58 +0530

    added searching page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,28 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import Lottie from "react-lottie";
+import searchData from "@/assets/searching.json";
+
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: searchData,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
+
 export default function Search() {
   return (
-    <div className="w-full p-4">
-      <h1>Search Page</h1>
+    <div className="w-full h-screen flex flex-col justify-center items-center p-4">
+      <div className="w-full">
+        <Lottie options={defaultOptions} />
+      </div>
+      <p className="text-muted-foreground text-center mb-8">
+        Searching for Partner...
+      </p>
+      <Button className="w-full">Cancel</Button>
     </div>
   );
 }
