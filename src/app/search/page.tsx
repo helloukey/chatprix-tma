569ab6bff92386cac4222cf9531eb38bc13a7a52
Kunal <kunalukey32@gmail.com>
2025-01-30 17:08:52 +0530

    added back lottie search with condition

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-// import { SearchLottie } from "@/screens/search";
+import { SearchLottie } from "@/screens/search";
 
 export default function Search() {
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center p-4">
-      {/* <SearchLottie /> */}
+      <SearchLottie />
       <p className="text-muted-foreground text-center mb-8">
         Searching for Partner...
       </p>
--- src/screens/search.tsx
+++ src/screens/search.tsx
@@ -4,12 +4,14 @@ import Lottie from "react-lottie";
 import searchData from "@/assets/searching.json";
 
 export const SearchLottie = () => {
+  if (typeof window === "undefined") return null;
+
   const defaultOptions = {
     loop: true,
     autoplay: true,
     animationData: searchData,
   };
-  
+
   return (
     <div className="w-full">
       <Lottie options={defaultOptions} />
