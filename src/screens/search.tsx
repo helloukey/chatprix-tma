ff310759f6409560e89e93607f68e391d48591c7
Kunal <kunalukey32@gmail.com>
2025-01-30 17:51:11 +0530

    added dynamic import for lottie-react package

--- src/screens/search.tsx
+++ src/screens/search.tsx
@@ -1,5 +1,6 @@
-import Lottie from "lottie-react";
+import dynamic from "next/dynamic";
 import searchData from "@/assets/searching.json";
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 
 export const LottieSearch = () => {
   return (
