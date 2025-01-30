f4a0aaaef76eecbbe43f73e4c1fb82ec702c805b
Kunal <kunalukey32@gmail.com>
2025-01-30 19:19:54 +0530

    added max height for chat screen

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -4,7 +4,7 @@ import { Action, Header } from "@/screens/chat";
 
 export default function Chat() {
   return (
-    <div className="w-full h-screen flex flex-col justify-between items-center">
+    <div className="w-full h-screen max-h-screen flex flex-col justify-between items-center">
       <Header />
       <Action />
     </div>
