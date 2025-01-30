0fb31d1947abefb90a2efb4d5e40911bb2496c97
Kunal <kunalukey32@gmail.com>
2025-01-30 16:46:35 +0530

    added placeholder chat page

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,7 @@
+export default function Chat() {
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center p-4">
+      <p className="text-muted-foreground text-center mb-8">Chat Page</p>
+    </div>
+  );
+}
