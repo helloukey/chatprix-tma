b4ff341dd57a40f29d288777b403db3af1e848e1
Kunal <kunalukey32@gmail.com>
2025-01-31 14:05:33 +0530

    added firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDbYqPeqvBj04Y9XtN2BUamGyncbNNzInw",
+  authDomain: "chatprix-tma.firebaseapp.com",
+  projectId: "chatprix-tma",
+  storageBucket: "chatprix-tma.firebasestorage.app",
+  messagingSenderId: "139032993045",
+  appId: "1:139032993045:web:4908f55dc7ddb7c37c1d1c",
+  measurementId: "G-84V4PMWBHF",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export { app, analytics };
