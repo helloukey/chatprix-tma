951a30793a9637ca6ea4550dea7b38e6d88edf7b
Kunal <kunalukey32@gmail.com>
2025-02-02 18:31:11 +0530

    added launchparam at root level

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,11 +2,14 @@
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ComponentProps } from "react";
+import { retrieveLaunchParams } from "@telegram-apps/sdk-react";
 
 export function ThemeProvider({
   children,
   ...props
 }: ComponentProps<typeof NextThemesProvider>) {
+  const data = retrieveLaunchParams();
+  console.log(data);
 
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
