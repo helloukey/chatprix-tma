fd94b1adcde9f44ac5e83fd42c44cabcfb71fcb4
Kunal <kunalukey32@gmail.com>
2025-02-02 13:25:45 +0530

    added user init data in useEffect

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,14 +1,16 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { ComponentProps } from "react";
+import { ComponentProps, useEffect } from "react";
 import { initDataUser } from "@telegram-apps/sdk-react";
 
 export function ThemeProvider({
   children,
   ...props
 }: ComponentProps<typeof NextThemesProvider>) {
-  const user = initDataUser();
-  console.log("user", user);
+  useEffect(() => {
+    const user = initDataUser();
+    console.log("user", user);
+  }, []);
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
