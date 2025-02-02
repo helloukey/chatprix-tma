12d7411f7154913b01e74a53c758411704872449
Kunal <kunalukey32@gmail.com>
2025-02-02 16:29:54 +0530

    added usesignal

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,14 +2,14 @@
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ComponentProps } from "react";
-import { initData } from "@telegram-apps/sdk-react";
+import { initData, useSignal } from "@telegram-apps/sdk-react";
 
 export function ThemeProvider({
   children,
   ...props
 }: ComponentProps<typeof NextThemesProvider>) {
-    const user = initData.user();
-    console.log(user);
+  const data = useSignal(initData.user);
+  console.log(data);
 
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
