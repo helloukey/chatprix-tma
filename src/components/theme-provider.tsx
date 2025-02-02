088932bbaec31193b3ff5767417ce1e6f1e69bab
Kunal <kunalukey32@gmail.com>
2025-02-02 16:32:37 +0530

    console.log istma

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,14 +2,13 @@
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ComponentProps } from "react";
-import { initData, useSignal } from "@telegram-apps/sdk-react";
+import { isTMA } from "@telegram-apps/sdk-react";
 
 export function ThemeProvider({
   children,
   ...props
 }: ComponentProps<typeof NextThemesProvider>) {
-  const data = useSignal(initData.user);
-  console.log(data);
+  console.log(isTMA());
 
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
