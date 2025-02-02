20045495b22d816a463106120ffa94e8e71d5b4b
Kunal <kunalukey32@gmail.com>
2025-02-02 18:18:33 +0530

    added user store

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,13 +2,11 @@
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ComponentProps } from "react";
-import { isTMA } from "@telegram-apps/sdk-react";
 
 export function ThemeProvider({
   children,
   ...props
 }: ComponentProps<typeof NextThemesProvider>) {
-  console.log(isTMA());
 
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
---
+++ src/zustand/useStore.ts
@@ -0,0 +1,18 @@
+import { create } from "zustand";
+import { DocumentData } from "firebase/firestore";
+
+interface UserState {
+  user: DocumentData | null;
+  setUser: (user: DocumentData | null) => void;
+  loading: boolean;
+  setLoading: (loading: boolean) => void;
+}
+
+const useUserState = create<UserState>()((set) => ({
+  user: null,
+  setUser: (user) => set({ user }),
+  loading: true,
+  setLoading: (loading) => set({ loading }),
+}));
+
+export { useUserState };
