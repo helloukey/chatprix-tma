bae6e93fca0bf4fd0ea15fcac025e2e0eb928102
Kunal <kunalukey32@gmail.com>
2025-02-02 16:18:21 +0530

    added invokation

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -8,7 +8,7 @@ export function ThemeProvider({
   children,
   ...props
 }: ComponentProps<typeof NextThemesProvider>) {
-    const user = initData.user;
+    const user = initData.user();
     console.log(user);
 
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
