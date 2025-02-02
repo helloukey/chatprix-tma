604c04844973c252bda8dcc90dd4fa53fc55798f
Kunal <kunalukey32@gmail.com>
2025-02-02 13:29:42 +0530

    removed init data user

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,16 +1,11 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { ComponentProps, useEffect } from "react";
-import { initDataUser } from "@telegram-apps/sdk-react";
+import { ComponentProps } from "react";
 
 export function ThemeProvider({
   children,
   ...props
 }: ComponentProps<typeof NextThemesProvider>) {
-  useEffect(() => {
-    const user = initDataUser();
-    console.log("user", user);
-  }, []);
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
