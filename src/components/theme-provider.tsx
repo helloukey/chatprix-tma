1a5d0680e456d0ce882325cb387eb57e0aabc3fc
Kunal <kunalukey32@gmail.com>
2025-02-02 13:20:37 +0530

    added temp initdatauser

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,10 +2,13 @@
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ComponentProps } from "react";
+import { initDataUser } from "@telegram-apps/sdk-react";
 
 export function ThemeProvider({
   children,
   ...props
 }: ComponentProps<typeof NextThemesProvider>) {
+  const user = initDataUser();
+  console.log("user", user);
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
