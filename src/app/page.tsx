72ce77edf65084ff7b114e397357283f9dd64d1b
Kunal <kunalukey32@gmail.com>
2025-02-02 13:31:52 +0530

    added init data user to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 "use client";
 
 import { Footer, Hero, ParticlesWrapper, SettingsDrawer } from "@/screens/home";
+import { useEffect } from "react";
+import { initDataUser } from "@telegram-apps/sdk-react";
 
 export default function Home() {
+  useEffect(() => {
+    const user = initDataUser();
+    console.log(user);
+  }, [])
   return (
     <ParticlesWrapper>
       <div className="w-full h-full flex flex-col justify-between items-center gap-8 p-4">
