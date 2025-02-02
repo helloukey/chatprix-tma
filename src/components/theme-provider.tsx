3c9959f2708a2396470c0f5dd5e7993b9e82bd7b
Kunal <kunalukey32@gmail.com>
2025-02-02 18:32:55 +0530

    added launch param inside useEffect

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,15 +1,17 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { ComponentProps } from "react";
+import { ComponentProps, useEffect } from "react";
 import { retrieveLaunchParams } from "@telegram-apps/sdk-react";
 
 export function ThemeProvider({
   children,
   ...props
 }: ComponentProps<typeof NextThemesProvider>) {
-  const data = retrieveLaunchParams();
-  console.log(data);
+  useEffect(() => {
+    const data = retrieveLaunchParams();
+    console.log(data);
+  }, [])
 
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
