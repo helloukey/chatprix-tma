9f9f014f297498ccf86a45d4fff71f891f040987
Kunal <kunalukey32@gmail.com>
2025-02-03 09:08:43 +0530

    removed console log for user state

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -3,12 +3,8 @@
 import { Button } from "@/components/ui/button";
 import { ParticlesWrapper } from "@/screens/home";
 import { LottieSearch } from "@/screens/search";
-import { useUserState } from "@/zustand/useStore";
 
 export default function Search() {
-  const { user } = useUserState(state => state);
-  console.log(user);
-  
   return (
     <ParticlesWrapper>
       <div className="w-full h-full flex flex-col justify-center items-center p-4">
