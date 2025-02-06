52575745df82617fa1685e8e69bf6f597652c3d2
Kunal <kunalukey32@gmail.com>
2025-02-06 14:15:21 +0530

    added api route

--- src/app/api/bot/route.ts
+++ src/app/api/bot/route.ts
@@ -1,17 +1,43 @@
-export const dynamic = "force-dynamic";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Bot } from "grammy";
 
-export const fetchCache = "force-no-store";
+const bot = new Bot(process.env.TELEGRAM_BOT_TOKEN!);
 
-import { Bot, webhookCallback } from "grammy";
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    // Process a POST request
+    if (!process.env.TELEGRAM_BOT_TOKEN) {
+      res
+        .status(500)
+        .json({ success: false, message: "Credentials not found." });
+      return;
+    }
+    const title = "Chatprix PRO";
+    const description = "Get Chatprix PRO for a month";
+    const payload = "";
+    const provider_token = "";
+    const currency = "USD";
+    const price = [
+      {
+        label: "Chatprix PRO",
+        amount: 4.99,
+      },
+    ];
 
-const token = process.env.TELEGRAM_BOT_TOKEN;
-
-if (!token)
-  throw new Error("TELEGRAM_BOT_TOKEN environment variable not found.");
-
-const bot = new Bot(token);
-bot.on("message:text", async (ctx) => {
-  await ctx.reply(ctx.message.text);
-});
-
-export const POST = webhookCallback(bot, "std/http");
+    const invoiceLink = await bot.api.createInvoiceLink(
+      title,
+      description,
+      payload,
+      provider_token, // Provider token must to be empty for Telegram stars
+      currency,
+      price
+    );
+    res.status(200).json({ success: true, invoiceLink });
+  } else {
+    // Handle any other HTTP method
+    res.status(405).json({ success: false, message: "Method Not Allowed" });
+  }
+}
