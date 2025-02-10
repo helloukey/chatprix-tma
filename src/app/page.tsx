4dd92d09cb02fc22543ee9b425f0acf869f2ebb5
Kunal <kunalukey32@gmail.com>
2025-02-10 17:14:51 +0530

    removed spacing for component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,6 @@ import {
 export default function Home() {
   return (
     <ParticlesWrapper>
-      
       <div className="w-full h-full flex flex-col justify-between items-center gap-8 p-4">
         {/* Navbar */}
         <div className="w-full flex justify-end items-center">
