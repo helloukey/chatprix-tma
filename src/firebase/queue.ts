0671fb938546d562f81f831f290eabcb6319efde
Kunal Ukey <kunal@Kunals-MacBook-Air.local>
2025-02-12 12:18:49 -0800

    added console.log for query and snapshot

--- src/firebase/queue.ts
+++ src/firebase/queue.ts
@@ -101,7 +101,9 @@ const findMatchFromQueue = async (userId: string, user: DocumentData) => {
     const queuesRef = collection(db, "queues");
     const isFilter = user.filters ? true : false;
     const q = generateQuery(queuesRef, userId, user, isFilter);
+    console.log("q", q);
     const querySnapshot = await getDocs(q);
+    console.log("querySnapshot", querySnapshot);
     if (querySnapshot.empty) {
       return null;
     }
