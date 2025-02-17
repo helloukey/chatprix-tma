c17ae45c12004259bb587abc7f2b7e583aa6e537
Kunal Ukey <kunalukey32@gmail.com>
2025-02-17 00:24:31 -0800

    updated firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -9,12 +9,12 @@ import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "chatprix-tma.firebaseapp.com",
-  projectId: "chatprix-tma",
-  storageBucket: "chatprix-tma.firebasestorage.app",
-  messagingSenderId: "139032993045",
-  appId: "1:139032993045:web:4908f55dc7ddb7c37c1d1c",
-  measurementId: "G-84V4PMWBHF",
+  authDomain: "chatprix-tma-6c521.firebaseapp.com",
+  projectId: "chatprix-tma-6c521",
+  storageBucket: "chatprix-tma-6c521.firebasestorage.app",
+  messagingSenderId: "775599992642",
+  appId: "1:775599992642:web:a8e936c7d2cbdf9075e2db",
+  measurementId: "G-T5X774VPXE",
 };
 
 // Initialize Firebase
